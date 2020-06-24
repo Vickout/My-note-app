a6495150b0318bf5a906bbe620051ecdad880ad2
Victor Lourenco <vlourenco.m@gmail.com>
2020-06-24 18:42:32 -0300

    Auth Login

--- javascript_note_api/app/routes/users.js
+++ javascript_note_api/app/routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_TOKEN;
+
 const User = require('../models/user');
 
 router.post('/register', async (req, res) => {
@@ -10,6 +15,27 @@ router.post('/register', async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: 'Error registering new user' });
+  }
+})
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if(!user) {
+      res.status(401).json({ error: 'Incorrect email' });
+    } else {
+      user.isCorrectPassword(password, function(err, same) {
+        if(!same) {
+          res.status(401).json({ error: 'Incorrect password' });
+        } else {
+          const token = jwt.sign({ email }, secret, { expiresIn: '1d' });
+          res.json({ user: user, token: token });
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal error, please try again' });
   }
 })
 
