96e74be452fc0e66a6af7ebb8ed7b83190964ac6
Victor Lourenco <vlourenco.m@gmail.com>
2020-06-24 17:11:57 -0300

    Hashed Password

--- javascript_note_api/app/models/user.js
+++ javascript_note_api/app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let userSchema = new mongoose.Schema({
     name: String,
@@ -19,6 +20,21 @@ let userSchema = new mongoose.Schema({
         type: Date, 
         default: Date.now 
     },
+});
+
+userSchema.pre('save', function (next) {
+    if(this.isNew || this.isModified('password')) {
+        bcrypt.hash(this.password, 10,
+            (err, hashedPassword) => {
+                if(err) {
+                    next(err);
+                } else {
+                    this.password = hashedPassword;
+                    next();
+                }
+            }
+        )
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
