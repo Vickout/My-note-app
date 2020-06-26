a999f3d20ddfce14757db3d8c1417efcbde873f2
Victor Lourenco <vlourenco.m@gmail.com>
2020-06-26 17:16:22 -0300

    Create form

--- javascript_note/src/screens/auth/register/index.js
+++ javascript_note/src/screens/auth/register/index.js
@@ -1,9 +1,44 @@
 import React, { Fragment } from 'react';
+import { Column, Section, Title, Container, Card } from 'rbx';
+
+import Header from '../../../components/header';
+import RegisterForm from '../../../components/auth/register_form';
+import logoImage from '../../../assets/images/logo.png';
+
+import '../../../styles/auth.scss';
 
 export default function RegisterScreen() {
     return (
         <Fragment>
-            Hello World!!
+            <Header />
+            <Section size="medium" className="auth">
+                <Container>
+                    <Column.Group centered>
+                        <Column size={3}>
+                            <Card>
+                                <Card.Content>
+                                    <Section>
+                                        <Column.Group centered>
+                                            <Column size={12}>
+                                                <img src={logoImage} alt="Logo javascript notes" />
+                                            </Column>
+                                        </Column.Group>
+
+                                        <Column.Group>
+                                            <Column size={12}>
+                                                <Title size={6} className="has-text-grey has-text-centered">
+                                                    Your notes on the cloud
+                                                </Title>
+                                            </Column>
+                                        </Column.Group>
+                                        <RegisterForm />
+                                    </Section>
+                                </Card.Content>
+                            </Card>
+                        </Column>
+                    </Column.Group>
+                </Container>
+            </Section>
         </Fragment>
     );
 }
