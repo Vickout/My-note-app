5447e423de95824307f3af8c7cf728aca39ca231
Victor Lourenco <vlourenco.m@gmail.com>
2020-07-10 15:16:49 -0300

    Structuring notes page

---
+++ javascript_note/src/components/notes/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+export default function Notes(props) {
+  return(
+    <Fragment>
+      Notes...
+    </Fragment>
+  )
+}
--- javascript_note/src/screens/notes/index/index.js
+++ javascript_note/src/screens/notes/index/index.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react';
 import HeaderLogged from '../../../components/header_logged';
+import Notes from "../../../components/notes";
+
+import "../../styles/notes.scss";
 
 export default function NotesScreen() {
     return (
         <Fragment>
             <HeaderLogged />
-            Hello World!!
+            <Notes />
         </Fragment>
     );
 }
