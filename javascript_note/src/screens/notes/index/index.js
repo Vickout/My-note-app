f93aea127ad2182aca1190890a44ae5ff051b11f
Victor Lourenco <vlourenco.m@gmail.com>
2020-07-10 15:07:39 -0300

    Header Logout

--- javascript_note/src/screens/notes/index/index.js
+++ javascript_note/src/screens/notes/index/index.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
+import HeaderLogged from '../../../components/header_logged';
 
 export default function NotesScreen() {
     return (
         <Fragment>
+            <HeaderLogged />
             Hello World!!
         </Fragment>
     );
