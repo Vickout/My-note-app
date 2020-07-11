070113f70dc0acff40fa7f97fdfa04f3a47a887a
Victor Lourenco <vlourenco.m@gmail.com>
2020-07-11 18:31:36 -0300

    Sidebar

--- javascript_note/src/screens/notes/index/index.js
+++ javascript_note/src/screens/notes/index/index.js
@@ -1,14 +1,15 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import HeaderLogged from '../../../components/header_logged';
 import Notes from "../../../components/notes";
 
-import "../../styles/notes.scss";
+import "../../../styles/notes.scss";
 
 export default function NotesScreen() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <Fragment>
-            <HeaderLogged />
-            <Notes />
+            <HeaderLogged setIsOpen={setIsOpen} />
+            <Notes setIsOpen={setIsOpen} isOpen={isOpen} />
         </Fragment>
     );
 }
