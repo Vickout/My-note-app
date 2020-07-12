3685f75ef0a91b017feffef5ad4ed705dc70c7bc
Victor Lourenco <vlourenco.m@gmail.com>
2020-07-11 21:38:26 -0300

    Listing notes

--- javascript_note/src/components/notes/index.js
+++ javascript_note/src/components/notes/index.js
@@ -1,9 +1,34 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { Column, Button } from "rbx";
-import '../../styles/notes.scss';
 import { push as Menu } from 'react-burger-menu';
+import List from "../notes/list";
+import NoteService from '../../services/notes';
+
+import '../../styles/notes.scss';
 
 export default function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    })
+    setCurrentNote(note);
+  }
+
   return (
     <Fragment>
       <Column.Group className="notes" id="notes">
@@ -21,7 +46,10 @@ export default function Notes(props) {
               Search...
             </Column>
           </Column.Group>
-          <p>List...</p>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note} />
         </Menu>
 
 
